438379a01bd7a2e71e214db6b6891f427d3959c2
Sahil Nayak <sahilsony2002@gmail.com>
2026-09-05 10:48:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstWord(s) {
   // your code here
+	if(s===""){
+		return ""
+	}
+	for(i=0;i<s.length;i++){
+		if(s[i]===" "){
+			return s.slice[0,i]
+		}
+	}
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+/alert(firstWord(s));
